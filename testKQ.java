5748d24867a9421fcf2c27fc8446f55ddfd4df9e
ChauThien <125325770+ChauThien@users.noreply.github.com>
2023-02-24 09:08:53 +0700

    Add files via upload

---
+++ testKQ.java
@@ -0,0 +1,13 @@
+package tuan2;
+
+public class testKQ {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		tuan2 cdr = new tuan2();
+		cdr.tieuDe();
+		System.out.println(cdr);
+
+	}
+
+}
